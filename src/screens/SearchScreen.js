3aea9c23db5db6b2c2519912494a8c06416860a4
badtux66 <edyp@live.com>
2022-12-21 15:46:05 +0300

    l90

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,12 @@
+import react from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const SearchScreen = () => {
+  <View>
+    <Text>Search Screen</Text>
+  </View>
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
