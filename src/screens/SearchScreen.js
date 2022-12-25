0336ec4783f1ae446f7f66cb7deed35214d8c532
badtux66 <edyp@live.com>
2022-12-25 18:30:43 +0300

    l96

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,15 @@
-import react from "react";
+import react, {useState} from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState('');
+
   return (
     <View>
-    <SearchBar />
+    <SearchBar term={term} onTermChange={newTerm => setTerm(newTerm)} />
     <Text>Search Screen</Text>
+    <Text>{term}</Text>
   </View>
   );
 };
