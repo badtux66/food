ef63b21e442abff1349a27aec643caeae7825f46
badtux66 <edyp@live.com>
2022-12-26 18:22:23 +0300

    l199

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,7 +4,7 @@ export default axios.create({
   baseURL: "https://api.yelp.com/v3/businesses",
   headers: {
     Authorization:
-      "Bearer ScolnSvKEa5WOChrHAVXTbrFsk-FsLgiOnnGwaxIYTswUf_A5ZhNt2IGHUs74udlNqJLDU0_EufE6k-wmfkmvA9sZlA-9RbkgHxSU5J_jWGLXxWtQptWhXNL1qOiY3Yx",
+      "BearerScolnSvKEa5WOChrHAVXTbrFsk-FsLgiOnnGwaxIYTswUf_A5ZhNt2IGHUs74udlNqJLDU0_EufE6k-wmfkmvA9sZlA-9RbkgHxSU5J_jWGLXxWtQptWhXNL1qOiY3Yx",
   },
 });
 
