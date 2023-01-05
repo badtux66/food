d8cc1a8ec46ac4c3abb999f61f4d157bbb259aae
badtux66 <edyp@live.com>
2023-01-05 21:33:53 +0300

    l107

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -8,6 +8,15 @@ const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [searchApi, results, errorMessage] = useResults();
 
+  const filterResultsByPrice = (price) => {
+    // price === '$' || '$$' || '$$$'
+    return results.filter(result => {
+      return result.price === price;
+    })
+  };
+
+  console.log(results);
+
   return (
     <View>
       <SearchBar 
@@ -17,9 +26,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
-      <ResultsList title="Cost Effective" />
-      <ResultsList title="Bit Pricier" />
-      <ResultsList title="Big Spender" />
+      <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
+      <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+      <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
     </View>
   );
 };
