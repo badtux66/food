9778caee514c8ab1c082ad18f297a09568c405c8
badtux66 <edyp@live.com>
2023-01-05 22:00:01 +0300

    l109

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -3,10 +3,21 @@ import { View, Image, Text, StyleSheet } from "react-native";
 
 const ResultsDetail = ({ result }) => {
   return <View>
+    <Image style={Styles.image} source={{ uri: result.image_url }} />
     <Text>{result.name}</Text>
+    <Text>{result.rating} Stars, {result.review_count} Reviews</Text>
   </View>
 };
 
-const Styles = StyleSheet.create({});
+const Styles = StyleSheet.create({
+  image: {
+    width: 250,
+    height: 120,
+    borderRadius: 4
+  },
+  name: {
+    fontWeight: 'bold',
+  }
+});
 
 export default ResultsDetail;
