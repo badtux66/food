784d6b32409105cb6980ae9bb152cf412daf9e33
badtux66 <edyp@live.com>
2023-01-07 21:54:16 +0300

    l114

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -25,7 +25,7 @@ const SearchScreen = () => {
         onTermSubmit={() => searchApi(term)}
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
-      <Text>We have found {results.length} results</Text>
+      
       <ScrollView>
       <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
       <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
